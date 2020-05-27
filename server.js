6c0c2ae3d2a5a563cd191db520eee4e46f39e5ba
NMHIEU2021999 <minhhieu2021999@gmail.com>
2020-05-28 06:43:02 +0700

    update

--- server.js
+++ server.js
@@ -3,24 +3,26 @@
 var express = require('express');
 var cors = require('cors');
 
-// require and use "multer"...
 var multer = require('multer');
 
 var app = express();
 
 app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './uploads');
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname);
+    cb(null, file.originalname);
   }
 })
  
 var upload = multer({ storage: storage })
-
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
   const file = req.file
   if (!file) {
